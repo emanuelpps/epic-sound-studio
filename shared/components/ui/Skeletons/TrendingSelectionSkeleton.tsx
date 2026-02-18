b2b06c92ec6e5548979eb281bea27ba6b4707e16
emanuelpps <emanuelpages.ps@gmail.com>
2026-02-18 20:34:28 -0300

    feat: added correction to skeleton loeader

--- shared/components/ui/Skeletons/TrendingSelectionSkeleton.tsx
+++ shared/components/ui/Skeletons/TrendingSelectionSkeleton.tsx
@@ -4,13 +4,14 @@ import { TrackCardSkeleton } from "@/shared/components/ui/Cards/TrackCardSkeleto
 
 export function TrendingSectionSkeleton() {
   return (
-    <section className="col-span-2 rounded-3xl bg-[#120914]/60 backdrop-blur-md border border-[#f91fc3]/15 p-6">
-      <div className="mb-6 space-y-3">
-        <div className="h-6 w-56 bg-[#2a1630] rounded-lg" />
-        <div className="h-4 w-40 bg-[#221326] rounded-lg opacity-70" />
+    <section className="relative col-span-2 rounded-3xl bg-gradient-to-br from-[#120914]/70 to-[#09050a]/80 backdrop-blur-md border border-[#f91fc3]/15 p-6 shadow-[0_0_50px_rgba(249,31,195,0.08)] mb-12">
+      <div className="flex justify-between w-full">
+        <div className="mb-6 relative pl-5 flex flex-col">
+          <div className="h-6 w-56 bg-[#2a1630] rounded-lg" />
+          <div className="h-4 w-40 bg-[#221326] rounded-lg opacity-70" />
+        </div>
       </div>
-
-      <div className="flex gap-6">
+      <div className="flex gap-6 overflow-x-auto scroll-smooth no-scrollbar">
         {Array.from({ length: 5 }).map((_, i) => (
           <TrackCardSkeleton key={i} />
         ))}
